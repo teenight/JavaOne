9f91f74e75b8dc8f6a54fb7ae9863a9ac929ebb0
Nick Zhong <zzj21504991@gmail.com>
2017-02-14 05:01:39 -0600

    Add files via upload

---
+++ Project_01/JLetter.java
@@ -0,0 +1,25 @@
+/**
+ * A program to display letter J composed of the character 'J', 'A', 'V', 'A'.
+ *
+ * Project 01.
+ * @author Zejian Zhong - COMP 1210 - 008
+ * @version 1/21/2017
+ */
+public class JLetter {
+   /**
+    * Displays a big letter J.
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}         
