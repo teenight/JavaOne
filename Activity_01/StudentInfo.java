8a432ba46b6228674823572f1f2c6df39d8079d7
Nick Zhong <zzj21504991@gmail.com>
2017-02-14 04:46:45 -0600

    Add files via upload

---
+++ Activity_01/StudentInfo.java
@@ -0,0 +1,18 @@
+/**
+ * Simple program to print Student Info.
+ * 
+ * Activity 01.
+ * @author Zejian Zhong - COMP 1210 - 008
+ * @version 1/18/2017
+ */
+public class StudentInfo {
+   /**
+    * Prints Student Info one time.
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args) {
+      System.out.println("Name: Zejian Zhong");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   } 
+}
