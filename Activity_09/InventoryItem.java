0d7b138dd5abed72c59dc2d45d20234ba75fb34d
Nick Zhong <zzj21504991@gmail.com>
2017-04-03 23:04:45 -0500

    Add files via upload

---
+++ Activity_09/InventoryItem.java
@@ -0,0 +1,46 @@
+/**
+ * Inventory item class.
+ *
+ * Activity_09.
+ * @author Zejian Zhong - COMP 1210 - 008
+ * @version 4/3/2017
+ */
+public class InventoryItem {
+   protected String name;
+   protected double price;
+   private static double taxRate = 0;
+   
+   /**
+    * @param nameIn the input name.
+    * @param priceIn the input price.
+    */
+   public InventoryItem(String nameIn, double priceIn) {
+      name = nameIn;
+      price = priceIn;
+   }
+   
+   /**
+    * @return the name.
+    */
+   public String getName() {
+      return name;
+   }
+   /**
+    * @return the total cost.
+    */
+   public double calculateCost() {
+      return price * (1 + taxRate);
+   }
+   /**
+    * @param taxRateIn sets the tax rate.
+    */
+   public static void setTaxRate(double taxRateIn) {
+      taxRate = taxRateIn;
+   }
+   /**
+    * @return the name and cost as string.
+    */
+   public String toString() {
+      return name + ": $" + calculateCost();
+   }
+}
